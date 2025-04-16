2bdb573e6a2d9e308695f5ca3cddee50ba8bf0ef
AmandaJCampos <amandajcampos8@gmail.com>
2025-04-16 14:47:31 -0300

    Concluído a busca do CEP com Api VIACEP

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 
-document.getElementById("btnBuscarCEP").addEventListener('click', (e) => {
-    e.preventDefault()
+document.getElementById("btnBuscarCEP").addEventListener('click', () => {
     const cep = document.getElementById('cep').value;
-    // console.log(cep);
 
     // Buscar o CEP
     buscarCEP(cep)
@@ -10,18 +8,30 @@ document.getElementById("btnBuscarCEP").addEventListener('click', (e) => {
 
 function buscarCEP(cep) {
     // const link = 'https://viacep.com.br/ws/01001000/json/';
-    const link = 'https://viacep.com.br/ws/' + cep + '/json/';
-    // Envia o CEP informado para a API do ViaCEP
+    const link = 'https://viacep.com.br/ws/' + cep + '/json/'; //OPÇÃO 1 
+    // const linkk = `https://viacep.com.br/ws/${cep}/json/`; --> OPÇÃO 2
+
+    // Envia o CEP informado para a API do ViaCEP (Webservice externo)
     fetch(link)
+    // Se tiver sucesso, retorna a resposta com os valores
     .then(
+        //Converte os valores para formato JSON
         response => response.json()
     )
+    // Preenche os campos do endereço
     .then(
-        dados => {
-            console.log(dados);
-            document.getElementById('logradouro').value = dados.logradouro;
-            document.getElementById('bairro').value = dados.bairro;
-
-        }
+       dados => {
+        //Preenche os campos de endereço
+            preencherDados(dados)
+       }
     )
+}
+
+function preencherDados(dados){
+        document.getElementById('logradouro').value = dados.logradouro;
+        document.getElementById('bairro').value = dados.bairro;
+        document.getElementById('localidade').value = dados.localidade;
+        document.getElementById('uf').value = dados.uf;
+        document.getElementById('regiao').value = dados.regiao;
+
 }
