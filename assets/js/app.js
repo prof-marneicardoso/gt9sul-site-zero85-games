a96bc68e50fe5183da6a89e4855bc4405e03a6e9
Eunhoj <john.vieira99@gmail.com>
2025-04-16 14:45:16 -0300

    Update do cadastro

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,23 @@
-document.getElementById("btnBuscarCEP").addEventListener('click', (e) => {
-    e.preventDefault()
-    const cep = document.getElementById('cep').value;
-    buscarCEP(cep)
+document.getElementById("btnBuscarCEP").addEventListener("click", () => {
+    const cep = document.getElementById("cep").value;
+    buscarCEP(cep);
 });
 
-function buscarCEP(cep)  {
+function buscarCEP(cep) {
     // Envia o cep informado para a API do ViaCEP
-    const link = 'https://viacep.com.br/ws/' + cep + '/json/';
+    const link = "https://viacep.com.br/ws/" + cep + "/json/";
     fetch(link)
-    .then(
-        response => response.json()
-    )
-    .then(
-        dados => {
-            console.log(dados);
-            document.getElementById('logradouro').value = dados.logradouro;
-            document.getElementById('bairro').value = dados.bairro;
-            document.getElementById('localidade').value = dados.localidade;
-            document.getElementById('estado').value = dados.estado;
-            document.getElementById('uf').value = dados.uf;
-            document.getElementById('regiao').value = dados.regiao;
-        }
-    )
+        .then((response) => response.json())
+        .then(
+            dados => preencherDados(dados)
+        );
+}
+
+function preencherDados(dados) {
+    document.getElementById("logradouro").value = dados.logradouro;
+    document.getElementById("bairro").value = dados.bairro;
+    document.getElementById("localidade").value = dados.localidade;
+    // document.getElementById("estado").value = dados.estado;
+    document.getElementById("uf").value = dados.uf;
+    document.getElementById("regiao").value = dados.regiao;
 }
