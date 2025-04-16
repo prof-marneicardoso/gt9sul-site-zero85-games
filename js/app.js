c2bcc10198afb2d1b7cf7c69265289bee783ca5f
adrianolima <sergiolima2504@gmail.com>
2025-04-16 15:36:24 -0300

    Finalizado

--- js/app.js
+++ js/app.js
@@ -11,17 +11,29 @@ document.getElementById("btnBuscarCEP").addEventListener('click', (e) => {
 function buscarCEP(cep) {
     // const link = 'https://viacep.com.br/ws/01001000/json/';
     const link = 'https://viacep.com.br/ws/' + cep + '/json/';
-    // Envia o CEP informado para a API do ViaCEP
-    fetch(link)
-    .then(
-        response => response.json()
-    )
-    .then(
-        dados => {
-            console.log(dados);
-            document.getElementById('logradouro').value = dados.logradouro;
-            document.getElementById('bairro').value = dados.bairro;
 
-        }
-    )
-}
+    // Envia o CEP informado para a API do ViaCEP (WebService externo)
+        fetch(link)
+        // Se tiver sucesso, retorna a resposta com os valores.
+    .then(
+            // Converte os valores para o formato JSON
+     )
+     .then(
+         dados => {
+             console.log(dados);
+             document.getElementById('logradouro').value = dados.logradouro;
+             document.getElementById('bairro').value = dados.bairro;
+ 
+         }
+         // Preenche os campos do endereço
+             
+        )
+ }
+  
+ function preencherDados(dados) {    
+     document.getElementById('logradouro').value = dados.logradouro;
+     document.getElementById('bairro').value = dados.bairro;
+     document.getElementById('localidade').value = dados.localidade;
+     document.getElementById('uf').value = dados.uf;
+     document.getElementById('regiao').value = dados.regiao;
+ }
