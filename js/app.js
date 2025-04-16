fe0ee550111a524d47629253c26bd8401494875b
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2025-04-16 14:42:41 -0300

    Finalizada a Busca do CEP com API ViaCEP

--- js/app.js
+++ js/app.js
@@ -1,27 +1,30 @@
 
-document.getElementById("btnBuscarCEP").addEventListener('click', (e) => {
-    e.preventDefault()
+document.getElementById("btnBuscarCEP").addEventListener('click', () => {
     const cep = document.getElementById('cep').value;
-    // console.log(cep);
-
-    // Buscar o CEP
-    buscarCEP(cep)
+    buscarCEP(cep);
 });
 
 function buscarCEP(cep) {
     // const link = 'https://viacep.com.br/ws/01001000/json/';
     const link = 'https://viacep.com.br/ws/' + cep + '/json/';
-    // Envia o CEP informado para a API do ViaCEP
+
+    // Envia o CEP informado para a API do ViaCEP (WebService externo)
     fetch(link)
     .then(
+        // Se tiver sucesso, retorna a resposta com os valores
         response => response.json()
+        // Converte os valores para o formato JSON
     )
     .then(
-        dados => {
-            console.log(dados);
-            document.getElementById('logradouro').value = dados.logradouro;
-            document.getElementById('bairro').value = dados.bairro;
-
-        }
+        // Preenche os campos do endereço
+        dados => preencherDados(dados)
     )
+}
+
+function preencherDados(dados) {    
+    document.getElementById('logradouro').value = dados.logradouro;
+    document.getElementById('bairro').value = dados.bairro;
+    document.getElementById('localidade').value = dados.localidade;
+    document.getElementById('uf').value = dados.uf;
+    document.getElementById('regiao').value = dados.regiao;
 }
